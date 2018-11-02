d31e7b8e28dae84c5cec1287f1cc76cca43ec0a5
docmojoman <docmojoman@gmail.com>
2018-11-02 01:02:42 -0400

    Tag feature added

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// Tags
-// Route::get('/admin/tags', 'Admin\TagsController@index');
-
-// Route::get('/admin/tags/fetch/{tag?}', 'Admin\TagsController@fetch');
-
