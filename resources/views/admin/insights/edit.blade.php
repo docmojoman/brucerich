df11d26140f7b8cea78bd57ec596815175eba0f9
docmojoman <docmojoman@gmail.com>
2019-01-15 16:29:30 -0500

    Insights backend tweaks

--- resources/views/admin/insights/edit.blade.php
+++ resources/views/admin/insights/edit.blade.php
@@ -19,7 +19,7 @@
             <label>Title:
               <input name="title" type="text" placeholder="Title" value="{{ $insight->title }}">
             </label>
-            <label>Author ( + coauthor if provided ):
+            <label>Author (+coauthor if provided):
               <input name="author" type="text" placeholder="Author" value="{{ $insight->author }}">
             </label>
             <label>Description:
